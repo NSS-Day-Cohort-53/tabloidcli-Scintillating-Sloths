b8a8a044764044ef4f252cfeecef0468116d0de6
JacobF94 <jacob.frensley@gmail.com>
2022-03-21 14:40:24 -0500

    progress

---
+++ TabloidCLI/UserInterfaceManagers/ColorManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class ColorManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine("1. List Journal Entries");
+            Console.WriteLine("2. Add a Journal Entry");
+            Console.WriteLine("3. Edit Journal Entry");
+            Console.WriteLine("4. Delete Journal Entry");
+            Console.WriteLine("0. Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
