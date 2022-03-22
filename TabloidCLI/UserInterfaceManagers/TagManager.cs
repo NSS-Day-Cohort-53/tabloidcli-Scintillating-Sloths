f43a5bca4ace7b9e566d369eae51bb4706df626a
cjgochanour <cjgochanour@gmail.com>
2022-03-22 09:35:54 -0500

    added a choose method to tagmanager

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -50,6 +50,36 @@ namespace TabloidCLI.UserInterfaceManagers
             }
         }
 
+        public Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Tag:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Blank Selection");
+                return null;
+            }
+        }
         private void List()
         {
             List<Tag> tags = _tagRepository.GetAll();
@@ -72,7 +102,7 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void Edit()
         {
-            throw new NotImplementedException();
+            Tag tagToEdit = Choose("Which tag would you like to edit?");
         }
 
         private void Remove()
